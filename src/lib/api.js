935bb6182763b83283b133862db7a61f2cb6db67
村井謙太 <muraikenta0507@gmail.com>
2017-05-04 00:43:41 +0900

    Set auth data for each api request

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,23 +1,27 @@
 import axios from 'axios'
 import _ from 'lodash'
+import Cookies from 'js-cookie'
 
-const host = {
+const baseURL = {
   development: 'http://localhost:3100',
   test: 'http://localhost:3100',
   production: 'http://xxx.com',
 }[process.env.NODE_ENV]
 
-const api = axios.create({
-  baseURL: `${host}/api`,
-})
+const api = axios.create({baseURL})
 
 api.interceptors.request.use((config) => {
   config.data = _.snakecaseKeys(config.data)
+  const authData = Cookies.getJSON('authData')
+  if (authData) {
+    config.headers = {...config.headers, ..._.kebabCaseKeys(authData)}
+  }
   return config
 })
 
 api.interceptors.response.use((res) => {
   res.data = _.camelcaseKeys(res.data)
+  res.headers = _.camelcaseKeys(res.headers)
   return res
 })
 
