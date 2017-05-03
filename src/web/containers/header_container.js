936282dd6e272039306569b7d3e9ef2c9b0d2435
村井謙太 <muraikenta0507@gmail.com>
2017-05-03 16:19:43 +0900

    Add stylesheet

--- src/web/containers/header_container.js
+++ src/web/containers/header_container.js
@@ -3,13 +3,14 @@ import {connect} from 'react-redux'
 
 const styles = {
   nav: {borderBottom: '1px solid gray'},
+  logo: {margin: 0},
 }
 
 class Header extends React.PureComponent {
   render() {
     return (
       <nav style={styles.nav}>
-        <h1>Parc</h1>
+        <h1 style={styles.logo}>Parc</h1>
       </nav>
     )
   }
