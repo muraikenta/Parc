ecdee3aef37580b3b6736028612d278c11cee5be
somakihiro <aki.air@icloud.com>
2017-05-04 00:39:00 +0900

    Add modal of landing

--- src/web/containers/landing_container.js
+++ src/web/containers/landing_container.js
@@ -1,15 +1,44 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Modal from 'react-modal'
 
 class Landing extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isModalOpen: false,
+    }
+  }
+
+  openModal() {
+    this.setState({isModalOpen: true})
+  }
+
+  closeModal() {
+    this.setState({isModalOpen: false})
+  }
+
   render() {
+    console.log(this.state.isModalOpen)
     return (
       <div>
         <h2>エンジニアが集まる場所</h2>
+        <button
+          type='button'
+          onClick={this.openModal.bind(this)}
+        >
+          アカウント作成
+        </button>
+        <Modal
+          isOpen={this.state.isModalOpen}
+          onRequestClose={this.closeModal.bind(this)}
+          contentLabel='modal'
+        >
+          <div>hoge</div>
+        </Modal>
       </div>
     )
   }
-
 }
 
 export default connect()(Landing)
