8d9f9addb0eb3429e6a3c1ebb79ad5de62214fad
村井謙太 <muraikenta0507@gmail.com>
2017-05-03 09:53:00 +0900

    Hello World by Redux

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const _ = require('lodash')
+
+const validEnvs = ['development', 'test', 'production']
+if (!_.isEmpty(process.env.NODE_ENV) && !_.includes(validEnvs, process.env.NODE_ENV)) {
+  throw Error('process.env.NODE_ENV is invalid')
+}
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname + "/public/scripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["react", "es2015", "stage-0"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ],
+  devtool: 'inline-source-map'
+}
