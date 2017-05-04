0e08c36553b2805738c30800c9cd8b552f217638
chikuwa111 <take0shun1111@gmail.com>
2017-05-04 19:51:21 +0900

    Add retweet and fav icon

--- src/web/components/post.js
+++ src/web/components/post.js
@@ -1,18 +1,43 @@
 import React from 'react'
 
-const style = {
-  padding: '30px 0',
-  textAlign: 'center',
-  borderLeft: '1px solid #e6ecf0',
-  borderRight: '1px solid #e6ecf0',
-  borderBottom: '1px solid #e6ecf0',
+const styles = {
+  wrapper: {
+    padding: '30px 0 10px',
+    textAlign: 'center',
+    borderLeft: '1px solid #e6ecf0',
+    borderRight: '1px solid #e6ecf0',
+    borderBottom: '1px solid #e6ecf0',
+  },
+  iconWrapper: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    passingTop: 5,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  iconStatus: {
+    marginLeft: 7,
+    color: 'rgba(170, 184, 194, 1)',
+  },
 }
 
 class Post extends React.PureComponent {
   render() {
     return (
-      <div style={style}>
+      <div style={styles.wrapper}>
         {this.props.content}
+        <div style={styles.iconWrapper}>
+          <div style={{cursor: 'pointer'}}>
+            <img src='/images/retweet_icon.png' style={styles.icon}/>
+            <span style={styles.iconStatus}>1</span>
+          </div>
+          <div style={{cursor: 'pointer'}}>
+            <img src='/images/fav_icon.png' style={styles.icon}/>
+            <span style={styles.iconStatus}>2</span>
+          </div>
+        </div>
       </div>
     )
   }
