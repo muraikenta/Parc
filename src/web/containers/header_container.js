051ad8678dca1440a63b0971deb91248d98b455a
chikuwa111 <take0shun1111@gmail.com>
2017-05-04 18:16:13 +0900

    Fix conflict

--- src/web/containers/header_container.js
+++ src/web/containers/header_container.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-<<<<<<< HEAD
-import {Link} from 'react-router-dom'
-=======
 import PostFormModal from '../components/post_form_modal'
->>>>>>> master
 
 const styles = {
   nav: {
