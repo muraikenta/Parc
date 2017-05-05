05cf6db674c17ca927bf82fa12399eadc4aa9738
chikuwa111 <take0shun1111@gmail.com>
2017-05-05 20:10:13 +0900

    Add follow banner layout

--- src/web/components/userpage.js
+++ src/web/components/userpage.js
@@ -1,6 +1,33 @@
 import React from 'react'
 import UserInfo from '../components/user_info'
 
+const styles = {
+  followButton: {
+    position: 'relative',
+    display: 'inline-block',
+    padding: '0.25em 0.5em',
+    textDecoration: 'none',
+    color: '#fff',
+    background: '#03A9F4',
+    border: '1px solid #0f9ada',
+    borderRadius: 4,
+    boxShadow: 'inset 0 1px 0 rgba(255,255,255,0.2)',
+    textShadow: '0 1px 0 rgba(0,0,0,0.2)',
+  },
+  unfollowButton: {
+    position: 'relative',
+    display: 'inline-block',
+    padding: '0.25em 0.5em',
+    textDecoration: 'none',
+    color: '#fff',
+    background: '#03A9F4',
+    border: '1px solid #0f9ada',
+    borderRadius: 4,
+    boxShadow: 'inset 0 1px 0 rgba(255,255,255,0.2)',
+    textShadow: '0 1px 0 rgba(0,0,0,0.2)',
+  },
+}
+
 class UserPage extends React.PureComponent {
   onFollowClick(e) {
     e.preventDefault()
@@ -15,11 +42,29 @@ class UserPage extends React.PureComponent {
   render() {
     return (
       <div>
-        <div>
+        <div style={{textAlign: 'center'}} >
           {this.props.userInfo.following ? (
-            <a href='#' onClick={this.onUnfollowClick.bind(this)} >Unfollow</a>
+            <div>
+              <span style={{marginRight: 10}} >フォローしています</span>
+              <a
+                href='#'
+                onClick={this.onUnfollowClick.bind(this)}
+                style={styles.unfollowButton}
+              >
+                Unfollow
+              </a>
+            </div>
           ) : (
-            <a href='#' onClick={this.onFollowClick.bind(this)} >Follow</a>
+            <div>
+              <span style={{marginRight: 10}} >フォローしていません</span>
+                <a
+                  href='#'
+                  onClick={this.onFollowClick.bind(this)}
+                  style={styles.followButton}
+                >
+                  Follow
+                </a>
+            </div>
           )}
         </div>
         <UserInfo
