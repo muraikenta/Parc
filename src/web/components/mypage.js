672f1c8c4cfcf95251a9ffaf2afb77ad1e44f475
ymfn4685 <ymfn4685@gmail.com>
2017-05-05 11:53:20 +0900

    Move EditProfileModal from MyPageContainer to MyPageComponents

--- src/web/components/mypage.js
+++ src/web/components/mypage.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import UserInfo from '../components/user_info'
+import EditProfileFormModal from '../components/edit_profile_form_modal'
 
 class MyPage extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      isEditProfileFormModalOpen: false,
+    }
+  }
+
+  openEditProfileFormModal() {
+    if (this.state.isEditProfileFormModalOpen) return
+    this.setState({isEditProfileFormModalOpen: true})
+  }
+
+  closeEditProfileFormModal() {
+    if (!this.state.isEditProfileFormModalOpen) return
+    this.setState({isEditProfileFormModalOpen: false})
+  }
+
   render() {
     return (
       <div>
         <UserInfo
           {...this.props.userInfo}
+        />
+        <span onClick={this.openEditProfileFormModal.bind(this)}>プロフィール編集</span>
+        <EditProfileFormModal
+          isOpen={this.state.isEditProfileFormModalOpen}
+          onRequestClose={this.closeEditProfileFormModal.bind(this)}
         />
       </div>
     )
